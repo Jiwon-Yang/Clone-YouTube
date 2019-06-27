02b6d5b6cd04c5e191f2444946e220ea196378a4
Jiwon Yang <jiwonyang.tech@gmail.com>
2019-06-27 21:42:06 +0900

    Completed #4 Webpack

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ import bodyParser from "body-parser";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
-import videoRouter from  "./routers/videoRouter";
+import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-
 
 const app = express();
 
@@ -18,6 +17,7 @@ app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
 //디렉토리에서 파일을 보내주는 미들웨어
 // '/uploads'로 가면 'uploads'라는 디렉토리 내에 들어감
+app.use("/static", express.static("static"));
 
 app.use(cookieParser());
 app.use(bodyParser.json());
